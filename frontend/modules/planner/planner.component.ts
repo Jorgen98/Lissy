c25e913f3b865d4051185e3494b4edfe5d19683e
Adam Včelař <adamvcelar@gmail.com>
2026-02-07 17:31:27 +0100

    Add map and sidebar to planner module

--- frontend/modules/planner/planner.component.ts
+++ frontend/modules/planner/planner.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { ModuleConfig } from '../../src/app/app.component';
 import * as config from './config.json';
+import { MapComponent } from '../../src/app/map/map.component';
+import { ImportsModule } from '../../src/app/imports';
 
 @Component({
     selector: 'app-planner',
-    imports: [],
+    imports: [ImportsModule, MapComponent],
     templateUrl: './planner.component.html',
     styleUrl: './planner.component.css',
 })
 export class PlannerModule {
+
+    // JSON config file
     static modulConfig: ModuleConfig = config;
+    public config: ModuleConfig = config;
+
+    // Sidebar module choice
+    public moduleFocus: Number = 0;
 }

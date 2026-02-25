e359e4b36547bba07b4297b0670a0c33f40da147
Adam Včelař <adamvcelar@gmail.com>
2026-02-25 12:00:38 +0100

    Basic routing prototype for a trip request with exactly two points

--- backend/planner/routing.ts
+++ backend/planner/routing.ts
@@ -5,13 +5,35 @@
  * Function for routing and building a response to a trip request using the planner adapter.
  */
 
+import { TransportMode } from "../../frontend/modules/planner/types/TransportMode";
 import { RoutePlanner } from "./RoutePlanner";
 import { TripRequest } from "./types/TripRequest";
+import { TripSectionInfo } from "./types/TripSectionInfo";
 
 export async function planTrip(request: TripRequest, planner: RoutePlanner): Promise<any | null> {
-    // Call 'planner' for each trip section given in 'request'
-    // Build sections into full trip
-        // Or number of possible trips when the number of midpoints is low
 
-    // Return trip(s) or null if some error occurs
+    // Specific case where there are only two trip points in the plan
+    if (request.points.length === 2) {
+
+        // Convert the object containing which modes are used for the entire trip into an array of selected modes as strings
+        const globalModes = (Object.keys(request.modes.global) as TransportMode[])
+            .filter(mode => request.modes.global[mode]);
+
+        // Create object with internal representation of information needed for a section of the trip between two points
+        const sectionInfo: TripSectionInfo = {
+            pointA: request.points[0]!,
+            pointB: request.points[1]!,
+            modes: globalModes,
+            datetime: {
+                option: request.datetime.datetimeOption,
+                date: request.datetime.tripDate,
+                time: request.datetime.tripTime   
+            }
+        };
+
+        // Send the internal representation to the selected adapter
+        // The adapter translates the internal TripSectionInfo object into the representation its corresponding service needs
+        // TODO decide on adequate maximum number of options for the section (3 for now)
+        return await planner.getTripSection(sectionInfo, 3);
+    }
 }

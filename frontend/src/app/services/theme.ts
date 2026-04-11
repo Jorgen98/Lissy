831a5bbab19289d617128ce37de6aa35474a4306
AdamV1177 <adamvcelar@gmail.com>
2026-04-11 14:14:04 +0200

    Update theming to respect system them preferences

--- frontend/src/app/services/theme.ts
+++ frontend/src/app/services/theme.ts
@@ -15,7 +15,7 @@ import { BehaviorSubject } from 'rxjs';
 export class ThemeService {
 
     // Name of item key for storing the latest theme in local storage
-    private LOCAL_STORAGE_ITEM_NAME = "lissyTheme";
+    private readonly LOCAL_STORAGE_ITEM_NAME = "lissyTheme";
 
     // Whether themes are currently in use by the shown module
     private useThemes = false;
@@ -25,6 +25,9 @@ export class ThemeService {
 
     // Same as above, turned into an observable that can be subscribed to 
     public isDark$ = this.isDark.asObservable();
+
+    // Prefered system color scheme
+    private mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
     // Getter, whether themes are enabled in the module
     get themesEnabled() {
@@ -36,27 +39,55 @@ export class ThemeService {
         return this.isDark.value;
     }
     
+    constructor() {
+
+        // Start listening for system color scheme preference changes
+        this.mediaQuery.addEventListener("change", (event) => {
+            if (!this.useThemes)
+                return;
+
+            // Use system theme if not set in localstorage
+            const currentStored = localStorage.getItem(this.LOCAL_STORAGE_ITEM_NAME);
+            if (!currentStored)
+                this.isDark.next(event.matches);
+        });
+    }
+
     // Initialization function, enabled usage of themes when called
     public init() {
         this.useThemes = true;
 
         // If the light theme is stored in local storage, initialize with light theme instead, otherwise default to dark
-        const storageTheme = localStorage.getItem(this.LOCAL_STORAGE_ITEM_NAME);
-        if (storageTheme === "light")
-            Promise.resolve().then(() => this.isDark.next(false));  // Postpone until angular change detection cycle completes
+        const storageTheme = localStorage.getItem(this.LOCAL_STORAGE_ITEM_NAME) as "light" | "dark" | null;
+
+        Promise.resolve().then(() => this.initApplyTheme(storageTheme));  // Postpone until angular change detection cycle completes
     }
 
     // Function setting the value of the isDark Subject
-    public setIsDark(value: boolean) {
-        this.isDark.next(value);
-
-        // Store the selected theme in local storage
-        localStorage.setItem(this.LOCAL_STORAGE_ITEM_NAME, value ? "dark" : "light");
+    public setTheme(value: "light" | "dark" | "system") {
+        if (value === "system") {
+            localStorage.removeItem(this.LOCAL_STORAGE_ITEM_NAME);
+            this.isDark.next(this.mediaQuery.matches);
+        }
+        else {
+            localStorage.setItem(this.LOCAL_STORAGE_ITEM_NAME, value);
+            this.isDark.next(value === "dark");
+        }
     }
 
     // Function reseting the theme service to default dark theme
     public setDefault() {
         this.useThemes = false;
         this.isDark.next(true);
+    }
+
+    // Function applying the selected theme in init() function
+    private initApplyTheme(value: "light" | "dark" | null) {
+        if (value === "light")
+            this.isDark.next(false);
+        else if (value === "dark")
+            this.isDark.next(true);
+        else 
+            this.isDark.next(this.mediaQuery.matches)
     }
 }

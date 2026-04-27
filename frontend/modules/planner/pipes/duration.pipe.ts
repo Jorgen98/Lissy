54f8fe2d7a6879b001341ca07237ec25051bda24
AdamV1177 <adamvcelar@gmail.com>
2026-04-27 10:00:46 +0200

    Round seconds in frontend duration pipe

--- frontend/modules/planner/pipes/duration.pipe.ts
+++ frontend/modules/planner/pipes/duration.pipe.ts
@@ -13,21 +13,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DurationPipe implements PipeTransform {
     transform(seconds: number): string {
 
+        const ceiledSeconds = Math.ceil(seconds); 
+
         // Format to seconds if the duration is less than a minute
         if (seconds < 60)
-            return `${seconds} s`;
+            return `${ceiledSeconds} s`;
 
         // Format to minutes if the duration is less than an hour
         // Also add seconds if its less than three minutes
-        const minutes = Math.floor(seconds / 60);
+        const minutes = Math.floor(ceiledSeconds / 60);
         if (minutes < 3)
-            return `${minutes} min ${seconds % 60} s`;
+            return `${minutes} min ${ceiledSeconds % 60} s`;
         else if (minutes < 60)
             return `${minutes} min`;
 
         // Otherwise format to hh:mm
         const hours = Math.floor(minutes / 60);
-        const modMins = Math.floor((seconds % 3600) / 60);
+        const modMins = Math.floor((ceiledSeconds % 3600) / 60);
         return `${hours} h ${modMins} min`;
     }
 }
